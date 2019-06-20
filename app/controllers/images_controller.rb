f85b36bbad2726d12e31cccc0dcf09e3e7bb6bdd
Rajasri Nanduri <rajasri.nanduri@appfolio.com>
2019-06-20 13:41:36 -0700

    RN-Added views and updated controller actions for images

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,22 @@
+class ImagesController < ApplicationController
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(params.require(:image).permit(:url))
+    if @image.save
+      redirect_to @image
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  def index
+    @images = Image.all
+  end
+end
