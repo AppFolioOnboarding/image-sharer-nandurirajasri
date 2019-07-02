4db5a7f3b511a03f06160faa43af16d2c4d572f8
Rajasri Nanduri <rajasri.nanduri@appfolio.com>
2019-07-01 18:04:42 -0700

    RN-Added Feedback Store

--- app/javascript/image_sharing/index.js
+++ app/javascript/image_sharing/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'mobx-react';
-import { FeedbackStore } from './stores/FeedbackStore';
+import FeedbackStore from './stores/FeedbackStore';
 import App from './components/App';
 
 const stores = {
